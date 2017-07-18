e6b81b90c43af769f42fe260639bccbbe00953cd
bivasmaiti26 <bivasmaiti26@gmail.com>
2017-07-18 14:13:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -35,6 +35,46 @@ struct node *leftRotate(struct node *x)
 	y->leftChild = x;
 	return y;
 }
+unsigned int ip_to_int (const char * ip)
+{
+    /* The return value. */
+    unsigned v = 0;
+    /* The count of the number of bytes processed. */
+    int i;
+    /* A pointer to the next digit to process. */
+    const char * start;
+
+    start = ip;
+    for (i = 0; i < 4; i++) {
+        /* The digit being processed. */
+        char c;
+        /* The value of this byte. */
+        int n = 0;
+        while (1) {
+            c = * start;
+            start++;
+            if (c >= '0' && c <= '9') {
+                n *= 10;
+                n += c - '0';
+            }
+            /* We insist on stopping at "." if we are still parsing
+               the first, second, or third numbers. If we have reached
+               the end of the numbers, we will allow any character. */
+            else if ((i < 3 && c == '.') || i == 3) {
+                break;
+            }
+            else {
+                return INVALID;
+            }
+        }
+        if (n >= 256) {
+            return INVALID;
+        }
+        v *= 256;
+        v += n;
+    }
+    return v;
+}
 // This function brings the data at root if key is present in tree.
 // If key is not present, then it brings the last accessed item at
 // root. This function modifies the tree and returns the new root
